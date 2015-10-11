ff5ffe72ee7e3952d5b7a67eab886d00890f222c
roylo <joeblack1986@gmail.com>
2015-10-11 15:50:16 +0800

    test comment

---
+++ DZD2/SwiftSinchRTC-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SwiftSinchRTC-Bridging-Header.h
+//  DZD2
+//
+//  Created by roylo on 2015/10/9.
+//  Copyright © 2015年 ChuroCat. All rights reserved.
+//
+
+#ifndef SwiftSinchRTC_Bridging_Header_h
+#define SwiftSinchRTC_Bridging_Header_h
+
+
+#endif /* SwiftSinchRTC_Bridging_Header_h */
